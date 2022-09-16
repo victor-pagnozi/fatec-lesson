e6f8d2e4187b7c64ad6967971974f8a36936cd05
victor-pagnozi <victor.pagnozi@gmail.com>
2022-09-15 23:12:47 -0300

    Finisheeeeeeeeeeeeeeeeeeeeeeed

--- index.js
+++ index.js
@@ -5,19 +5,75 @@ const formValue = document.getElementsByClassName('form-control');
 document.getElementById('fields-to-complete').innerText +=
   ' ' + formValue.length;
 
-function save(event) {
-  event.preventDefault();
-
-  toastr.success('Are you the 6 fingered man?');
-}
-
 function populateDueDate() {
   const selectDueDate = document.getElementById('dueDate');
   const options = [5, 10, 15, 20, 25];
 
   options.forEach((option) => {
-    selectDueDate.innerHTML += `<option value="${option}">${option}</option>`;
-  }); //substituir por map para exemplo
+    // selectDueDate.innerHTML += `<option value="${option}">${option}</option>`;
+  });
+
+  const selectDueDateMapped = options.map(
+    (option) => `<option value="${option}">${option}</option>`
+  );
+
+  selectDueDate.innerHTML = selectDueDateMapped;
+}
+
+function validatePrice(event) {
+  const value = event.target.value;
+  if (value < 0) {
+    event.target.value = 0;
+    return;
+  }
+}
+
+function showTotalPrice() {
+  const price = parseInt(document.getElementById('price').value) | 0;
+  const installation =
+    parseInt(document.getElementById('installation').value) | 0;
+
+  document.getElementById('totalPrice').innerHTML = price + installation;
+}
+
+function saveOnlyForExample(event) {
+  event.preventDefault();
+  const form = document.getElementById('formPaymentInformation');
+  const formElements = Array.from(form.elements);
+
+  const lengthOfFormElements = formElements.length;
+  const elementsMapped = formElements.map((element, index) => {
+    if (index < lengthOfFormElements - 2) {
+      return element.value;
+    }
+    return '';
+  });
+  console.log(elementsMapped);
+  toastr.success('Save Successfully!');
+}
+
+function save(event) {
+  event.preventDefault();
+  const paymentForm = {
+    name: document.getElementById('name').value,
+    email: document.getElementById('email').value,
+    cardNumber: document.getElementById('cardNumber').value
+  };
+
+  validateInputEmpty(paymentForm.name);
+
+  toastr.success(
+    `Congrats ${paymentForm.name}, Save Successfully!`,
+    'Success!'
+  );
+}
+
+function validateInputEmpty(input) {
+  if (!input) {
+    toastr.warning('Some input has been empty!', 'Warning');
+    return;
+  }
 }
 
 populateDueDate();
+showTotalPrice();
--- quokka.js
+++ quokka.js
@@ -1,4 +1,4 @@
-const array = ['1', '2', '3', '4', '5'];
+const array = [1, 2, 3, 4, 5];
 
 for (const item of array) {
   console.log(item);
@@ -28,3 +28,36 @@ const newArrayMapped = array.map((item) => {
   return item;
 });
 console.log(newArrayMapped);
+//**--------------------------------------------------------- */
+//Find e FindIndex
+//Find sempre retornará o primeiro valor encontrado pela condição especificada na função.
+//Find index é igual, mas ao invés de retornar o valor, irá retornar a posição em que o valor se encontra
+const par = array.find((number) => number % 2 === 0);
+const indexPar = array.findIndex((number) => number % 2 === 0);
+console.log(par);
+console.log(indexPar);
+
+//**--------------------------------------------------------- */
+const user = {
+  name: 'Victor',
+  age: 26,
+  occupation: 'Developer',
+  address: {
+    street: 'Rua X',
+    zipCode: 87901
+  }
+};
+
+console.log(user);
+
+console.log('name' in user);
+console.log(Object.keys(user));
+console.log(Object.values(user));
+
+//Desestruturação
+const { address, age: idade, occupation = 'Administration' } = user;
+console.log(address, idade, occupation);
+
+// console.log(user.hands.quantity)
+console.log(user.hands ? user.hands.quantity : 'Não informado');
+console.log(user.hands?.quantity ?? 'Não informado');
